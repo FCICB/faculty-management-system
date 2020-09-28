74303669fde085df2c3cd233b6cddd2247ab53de
menna <mennasayed192@gmail.com>
2020-09-28 05:01:32 +0200

    1- add fx as external library , add vm in configuration (to know ths javafx path).
    2- creat login form (login.fxml his controller :login), with two textfiled Email and password .if you write right data you will go to second page, if you write any thing wrong it will show you Alert error and return you in same page (login page).

--- src/com/fcicb/Main.java
+++ src/com/fcicb/Main.java
@@ -1,8 +1,31 @@
 package com.fcicb;
+//import sample.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.StageStyle;
 
-public class Main {
+public class Main extends Application {
 
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+     Parent root = FXMLLoader.load(getClass().getResource("../../sample/login.fxml"));
+
+       primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("FMS");
+       // primaryStage.setX(50);
+      //  primaryStage.setY(50);
+        primaryStage.show();
+
+
+    }
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        launch(args);
     }
 }
