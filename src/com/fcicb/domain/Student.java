8266f48bd41bd80e88bbaba0770c11bae4619674
amrd-dawood <amr.mohamed.dawood@gmaill.com>
2020-10-02 11:14:07 +0200

    - register student with default value 12345678, - delete student record using his id - deactivate Student - reactivate student

---
+++ src/com/fcicb/domain/Student.java
@@ -0,0 +1,156 @@
+package com.fcicb.domain;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class Student {
+
+    private int id;
+    private String username;
+    private float gpa;
+    private Date birthDate;
+    private enum  studentState { ACTIVATED, DEACTIVATED };
+    private studentState state;
+    private int level;
+    private int completedHours;
+    private String fname;
+    private String lname;
+    private String email;
+    private String password;
+    private Date createdAt;
+    private int deletedBy;
+    private int addedBy;
+
+    public Student(int id, String username, float gpa, Date birthDate,studentState state, int level, int completedHours,
+                   String fname, String lname, String email, String password, Date createdAt, int deletedBy, int addedBy) {
+        this.id = id;
+        this.username = username;
+        this.gpa = gpa;
+        this.birthDate = birthDate;
+        this.state = state;
+        this.level = level;
+        this.completedHours = completedHours;
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.deletedBy = deletedBy;
+        this.addedBy = addedBy;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public studentState getStudentState(){
+        return state;
+    }
+
+    public void setStudentState(studentState state){
+        this.state = state;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getCompletedHours() {
+        return completedHours;
+    }
+
+    public void setCompletedHours(int completedHours) {
+        this.completedHours = completedHours;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(int deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public int getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(int addedBy) {
+        this.addedBy = addedBy;
+    }
+}
---
+++ src/com/fcicb/model/dao/impl/StudentDao.java
@@ -0,0 +1,134 @@
+package com.fcicb.model.dao.impl;
+
+
+import com.fcicb.domain.Student;
+import com.fcicb.jdbc.DatabaseConnection;
+import com.fcicb.model.dao.Dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class StudentDao implements Dao<Student> {
+
+    DatabaseConnection instance = DatabaseConnection.getInstance();
+
+    @Override
+    public boolean add(Student item){
+
+        int result ;
+
+        try (Connection connection = instance.getConnection()) {
+            PreparedStatement insert = connection.prepareStatement("INSERT INTO student  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+                insert.setInt(1, item.getId());
+                insert.setString(2, item.getUsername());
+                insert.setFloat(3, item.getGpa());
+                insert.setDate(4, (Date) item.getBirthDate());
+                insert.setString(5, String.valueOf(item.getStudentState()));
+                insert.setInt(6, item.getLevel());
+                insert.setInt(7, item.getCompletedHours());
+                insert.setString(8, item.getFname());
+                insert.setString(9, item.getLname());
+                insert.setString(10, item.getEmail());
+                insert.setDate(11, (Date) item.getCreatedAt());
+                insert.setInt(12, item.getDeletedBy());
+                insert.setInt(13, item.getDeletedBy());
+
+            result=insert.executeUpdate();
+
+            if(result != 0)
+            {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+
+    @Override
+    public boolean delete(Student student) {
+
+        int result ;
+
+        try (Connection connection = instance.getConnection()) {
+
+            PreparedStatement delete = connection.prepareStatement("DELETE FROM student  WHERE id = ? ");
+            delete.setInt(1, student.getId());
+
+            result = delete.executeUpdate();
+
+            if ( result != 0 )
+                return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deactivate(Student student){
+        int result;
+
+        try (Connection connection = instance.getConnection()) {
+
+            PreparedStatement deactivate = connection.prepareStatement("UPDATE  student  account_activated = 'Deactivated' WHERE id = ? ");
+            deactivate.setInt(1, student.getId());
+
+            result = deactivate.executeUpdate();
+
+            if ( result != 0 )
+                return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean reactivate(Student student){
+        int result;
+
+        try (Connection connection = instance.getConnection()) {
+
+            PreparedStatement reactivated = connection.prepareStatement("UPDATE  student  account_activated = 'Activated' WHERE id = ? ");
+            reactivated.setInt(1, student.getId());
+
+            result = reactivated.executeUpdate();
+
+            if ( result != 0 )
+                return true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean add(List<Student> items) {
+        return false;
+    }
+
+    @Override
+    public Student getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean update(Student item) {
+        return false;
+    }
+}
